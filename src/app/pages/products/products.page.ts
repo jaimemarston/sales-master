49d2798bb48c19da3d4dd0ac133d65114ca6e22c
jaimemarston <jaimemarston@gmail.com>
2019-03-13 10:58:14 -0500

    mod productos
    mod productos

--- src/app/pages/products/products.page.ts
+++ src/app/pages/products/products.page.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {PRODUCTS} from '../../data';
-import {AuthService} from '../../providers/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { PRODUCTS } from '../../data';
+import { AuthService } from '../../providers/auth.service';
 
 @Component({
     selector: 'app-products',
@@ -8,14 +8,31 @@ import {AuthService} from '../../providers/auth.service';
     styleUrls: ['./products.page.scss'],
 })
 export class ProductsPage implements OnInit {
+
     products = PRODUCTS;
     auth = AuthService.getUserAuthenticated();
     date = new Date();
-
+    qty: any;
     constructor() {
+        this.qty = 1;
     }
 
     ngOnInit() {
     }
+    // increment product qty
+    incrementQty() {
+        console.log(this.qty + 1);
+        this.qty += 1;
+    }
 
+    // decrement product qty
+    decrementQty() {
+        if (this.qty - 1 < 1) {
+            this.qty = 1;
+            console.log('1 ->' + this.qty);
+        } else {
+            this.qty -= 1;
+            console.log('2 ->' + this.qty);
+        }
+    }
 }
